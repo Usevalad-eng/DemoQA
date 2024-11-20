4ebaed0d9cd93dcb3d43e959f03644ef1af4d26d
Usevalad <avm012@gmail.com>
2024-11-20 11:53:15 +0000

    20nov2024

---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+    @BeforeAll
+    static void screenResolution(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void textBoxTest(){
+
+        open("/text-box");
+        $(".text-center").shouldBe(visible);
+        $("#userName").setValue("Vasia");
+        $("#userEmail").setValue("vasia@gmail.com");
+        $("#currentAddress").setValue("vasia street, 20");
+        $("#permanentAddress").setValue("vasia street, 21");
+        $("#submit").scrollTo().click();
+
+        System.out.println("iii");
+        $("#name").shouldHave(text("Vasia"));
+        $("#email").shouldHave(text("vasia@gmail.com"));
+        $("#output").$("#currentAddress").shouldHave(text("vasia street, 20"));
+        $("#output").$("#permanentAddress").shouldHave(text("vasia street, 21"));
+    }
+}
