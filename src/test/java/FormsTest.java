54a786d55e67d14b8032874391ab4f37542cd4c6
Usevalad <avm012@gmail.com>
2024-11-22 11:53:09 +0000

    22nov2024

--- src/test/java/FormsTest.java
+++ src/test/java/FormsTest.java
@@ -22,18 +22,6 @@ public class FormsTest {
         Configuration.browserSize = "1920x1080";
     }
 
-    /*@Test
-    void formsSimpleTest() {
-        open("/automation-practice-form");
-        $(".text-center").shouldBe(visible);
-        $("#firstName").setValue("Vasiliy");
-        $("#lastName").setValue("Strelnikov");
-        $x("//label[@class ='custom-control-label' and text() = 'Male']").click();
-        $$x("//input[@type = 'checkbox']").get(0).click(ClickOptions.withTimeout(Duration.ofSeconds(1)));
-        $("#userNumber").setValue("1234567890");
-        $("#submit").scrollTo().click();
-    }*/
-
     @Test
     void formsTest() {
         open("/automation-practice-form");
@@ -43,9 +31,25 @@ public class FormsTest {
         $x("//label[@class ='custom-control-label' and text() = 'Male']").click();
         $("#userNumber").setValue("1234567890");
         $$x("//input[@type = 'checkbox']").get(0).click(ClickOptions.withTimeout(Duration.ofSeconds(1)));
+        $("#subjectsInput").setValue("exam");
+        $("#userEmail").setValue("vas@mail.ru");
+        $("#currentAddress").setValue("address");
+
+        /*$(".css-1hwfws3").click();
+        $x("//div[@class = ' css-1uccc91-singleValue']").click();
+        $(".css-1hwfws3").click();
+        $x("//div[text() = 'Delhi']").click();*/
+
         $("#submit").scrollTo().click();
 
         Selenide.switchTo().defaultContent();
         $x("//*[@id='example-modal-sizes-title-lg']").shouldHave(text("Thanks for submitting the form"));
+        $x("//td[text() = 'Vasiliy Strelnikov']").shouldHave(text("Vasiliy Strelnikov"));
+        $x("//td[text() = '1234567890']").shouldHave(text("1234567890"));
+        $x("//td[text() = 'Male']").shouldHave(text("Male"));
+        $x("//td[text() = 'vas@mail.ru']").shouldHave(text("vas@mail.ru"));
+        $x("//td[text() = 'address']").shouldHave(text("address"));
+        //$x("//td[text() = 'exam']").shouldHave(text("exam"));
+        //$x("//td[text() = 'Sports']").shouldHave(text("Sports"));
     }
 }
