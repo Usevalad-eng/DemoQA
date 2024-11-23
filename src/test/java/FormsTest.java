ed2070cbb8666f36982d47f6a5fb2d43d21ec0da
Usevalad <avm012@gmail.com>
2024-11-23 12:14:20 +0000

    23nov2024

--- src/test/java/FormsTest.java
+++ src/test/java/FormsTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.io.File;
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -25,31 +26,41 @@ public class FormsTest {
     @Test
     void formsTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove();");
         $(".text-center").shouldBe(visible);
         $("#firstName").setValue("Vasiliy");
         $("#lastName").setValue("Strelnikov");
-        $x("//label[@class ='custom-control-label' and text() = 'Male']").click();
-        $("#userNumber").setValue("1234567890");
-        $$x("//input[@type = 'checkbox']").get(0).click(ClickOptions.withTimeout(Duration.ofSeconds(1)));
-        $("#subjectsInput").setValue("exam");
         $("#userEmail").setValue("vas@mail.ru");
+        $("#userNumber").setValue("1234567890");
+        $("#genterWrapper").$(byText("Male")).click();
+        //$x("//label[@class ='custom-control-label' and text() = 'Male']").click();
+        $("#dateOfBirthInput").scrollTo().click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2004");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        //$$x("//input[@type = 'checkbox']").get(0).click(ClickOptions.withTimeout(Duration.ofSeconds(1)));
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/1.png"));
+        $("#uploadPicture").uploadFromClasspath("1.png");
         $("#currentAddress").setValue("address");
-
-        /*$(".css-1hwfws3").click();
-        $x("//div[@class = ' css-1uccc91-singleValue']").click();
-        $(".css-1hwfws3").click();
-        $x("//div[text() = 'Delhi']").click();*/
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
 
         $("#submit").scrollTo().click();
 
-        Selenide.switchTo().defaultContent();
-        $x("//*[@id='example-modal-sizes-title-lg']").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //$x("//*[@id='example-modal-sizes-title-lg']").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Vasiliy"), text("Strelnikov"));
         $x("//td[text() = 'Vasiliy Strelnikov']").shouldHave(text("Vasiliy Strelnikov"));
         $x("//td[text() = '1234567890']").shouldHave(text("1234567890"));
         $x("//td[text() = 'Male']").shouldHave(text("Male"));
         $x("//td[text() = 'vas@mail.ru']").shouldHave(text("vas@mail.ru"));
         $x("//td[text() = 'address']").shouldHave(text("address"));
-        //$x("//td[text() = 'exam']").shouldHave(text("exam"));
-        //$x("//td[text() = 'Sports']").shouldHave(text("Sports"));
+        $x("//td[text() = 'Maths']").shouldHave(text("Maths"));
+        $x("//td[text() = 'Sports']").shouldHave(text("Sports"));
     }
 }
